341f4576c9b70605a453eb301f2ef0485118873e
OryzaAZ <zoryzaamaliatuz@gmail.com>
2023-12-03 01:40:44 +0700

    add unavailable-product

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -6,6 +6,7 @@ export default {
         return {
             products: [],
             currentIndex: 0,
+            hasUnavailable: false,
         }
     },
     computed: {
@@ -21,6 +22,43 @@ export default {
         async callProductAPI() {
             const productApi = await fetch('https://fakestoreapi.com/products')
             const products = await productApi.json()
+
+            // const mensClothingIndex = products.findIndex(product => product.category.toLowerCase() === 'men\'s clothing');
+            const womensClothingIndex = products.findIndex(product => product.category.toLowerCase() === 'women\'s clothing');
+
+
+            // for (const product of products) {
+            //     if (product.category.toLowerCase() === 'unavailable') {
+            //         hasUnavailable = true;
+            //         break;
+            //     }
+            // }
+            // Tambahkan satu produk "unavailable" di antara "mens clothing" dan "womens clothing"
+
+            const unavailableProduct = {
+                id: 999, // Atur id yang unik
+                title: 'Unavailable Product',
+                category: 'unavailable',
+                rating: 0,
+                description: 'This product is unavailable to show',
+                price: 0,
+                image: '@/assets/images/sad-face.svg', // Ganti dengan URL gambar sesuai kebutuhan
+                available: false,
+            };
+
+            products.forEach(product => {
+                product.available = product.category.toLowerCase() !== 'unavailable';
+            });
+            const repeatCount = 2;
+
+            for (let i = 0; i < repeatCount; i++) {
+                products.splice(womensClothingIndex, 0, { ...unavailableProduct, id: i + 999 });
+            }
+
+            this.hasUnavailable = products.some(product => !product.available);
+
+
+
             this.products = products
             console.log(products);
         },
@@ -30,7 +68,7 @@ export default {
         formatRating(rating) {
             return `${rating.rate}/5`
         },
-        formatTitle(category){
+        formatTitle(category) {
             if (category === 'men\'s clothing') {
                 return 'text-blue-dark'
             } else {
@@ -80,24 +118,24 @@ export default {
                 }
             }
         }
-//         formatButton(category) {
-//     if (category === "men's clothing") {
-//         return {
-//             'background-color': 'var(--blue-dark)',
-//             'border-color': 'var(--blue-dark)',
-//         };
-//     } else if (category === "women's clothing") {
-//         return {
-//             'background-color': 'var(--purple-dark)',
-//             'border-color': 'var(--purple-dark)',
-//         };
-//     } else {
-//         return {
-//             'background-color': 'var(--white)',
-//             'border-color': 'var(--white)',
-//         };
-//     }
-// }
+        //         formatButton(category) {
+        //     if (category === "men's clothing") {
+        //         return {
+        //             'background-color': 'var(--blue-dark)',
+        //             'border-color': 'var(--blue-dark)',
+        //         };
+        //     } else if (category === "women's clothing") {
+        //         return {
+        //             'background-color': 'var(--purple-dark)',
+        //             'border-color': 'var(--purple-dark)',
+        //         };
+        //     } else {
+        //         return {
+        //             'background-color': 'var(--white)',
+        //             'border-color': 'var(--white)',
+        //         };
+        //     }
+        // }
     },
     mounted() {
         this.callProductAPI()
@@ -111,19 +149,21 @@ export default {
             <div class="overlay">
                 <img src="../assets/images/bg-pattern.svg" alt="background men" class="overlay-background">
             </div>
-            <div class="product-card" v-for="product in [currentProduct]" :key="product.id">
-                <div class="product-info">
+
+            <div class="product-card" v-if="currentProduct.available">
+                <div class="product-info" v-for="product in [currentProduct]" :key="product.id">
                     <div class="product-image">
                         <img :src="product.image" alt="Product Image" />
                     </div>
                     <div class="product-detail">
-                        <div :class="['title', formatTitle(product.category)]" >
+                        <div :class="['title', formatTitle(product.category)]">
                             <h3>{{ product.title }}</h3>
                         </div>
                         <div class="sub-title">
                             <p> {{ product.category }}</p>
                             <p> {{ formatRating(product.rating) }}</p>
-                            <span v-for="i in 5" :key="i" :class="{'circle': true, 'filled': i <= Math.floor(currentProduct.rating.rate)}"></span>
+                            <span v-for="i in 5" :key="i"
+                                :class="{ 'circle': true, 'filled': i <= Math.floor(currentProduct.rating.rate) }"></span>
                         </div>
                         <div class="description">
                             <p>{{ product.description }}</p>
@@ -132,10 +172,27 @@ export default {
                             <p :class="formatPrice(product.category)">&dollar;{{ product.price }}</p>
                         </div>
                         <div class="button">
-                            <button :style="formatButtonBuy(product.category)" type="button"  class="btn-buy">Buy Now</button>
-                            <button :style="formatButtonNext(product.category)" type="button" @click="nextProduct" class="btn-next">Next Product</button>
+                            <button :style="formatButtonBuy(product.category)" type="button" class="btn-buy">Buy
+                                Now</button>
+                            <button :style="formatButtonNext(product.category)" type="button" @click="nextProduct"
+                                class="btn-next">Next Product</button>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div class="product-card" v-else>
+                <div class="unavailable-container">
+                    <div class="unavailable-image">
+                        <img src="@/assets/images/sad-face.svg" alt="">
+                    </div>
+                    <div class="product-detail-unavailable">
+                        <p>{{ currentProduct.description }}</p>
+                        <div class="button">
+                            <button :style="formatButtonNext(currentProduct.category)" type="button" @click="nextProduct"
+                                class="btn-next">Next Product</button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
